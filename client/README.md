5b68ee2d6f9c51b0da83a0df86944509d74cc0d0
shitoletushar3132 <shitoletushar3132@gmail.com>
2024-12-31 13:30:23 +0530

    starting

---
+++ client/README.md
@@ -0,0 +1,6 @@
+# DEV TINDER
+
+- created a vite + React application
+- Adding Tailwind css
+- Adding Daisy UI
+- Adding NavBar component
---
+++ sever/src/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+
+const connectDB = require("./config/database");
+
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+const authRouter = require("./routes/auth");
+const profileRouter = require("./routes/profile");
+const requestRouter = require("./routes/requests");
+const userRouter = require("./routes/user");
+
+app.use("/", authRouter);
+app.use("/", profileRouter);
+app.use("/", requestRouter);
+app.use("/", userRouter);
+
+connectDB()
+  .then(() => {
+    console.log("Database connection established...");
+    app.listen(3000, () => {
+      console.log("server is started at 3000");
+    });
+  })
+  .catch((err) => {
+    console.error("Database connect be connected");
+  });
