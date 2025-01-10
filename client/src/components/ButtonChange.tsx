c1460d4f6b6058f13c5f1ac90bbf8948f5168ccd
Tushar shitole <153802284+shitoletushar3132@users.noreply.github.com>
2025-01-10 08:58:20 +0530

    Update ButtonChange.tsx

--- client/src/components/ButtonChange.tsx
+++ client/src/components/ButtonChange.tsx
@@ -3,9 +3,10 @@ import { Link } from "react-router-dom";
 interface ButtonChangeProps {
   text: string;
   navigate: string;
+  text2: string;
 }
 
-const ButtonChange: React.FC<ButtonChangeProps> = ({ text, navigate }) => {
+const ButtonChange: React.FC<ButtonChangeProps> = ({ text, navigate,text2 }) => {
   return (
     <div className="flex gap-3">
       <p>{text}</p>
@@ -13,7 +14,7 @@ const ButtonChange: React.FC<ButtonChangeProps> = ({ text, navigate }) => {
         to={navigate}
         className="text-blue-500 underline hover:text-blue-700 transition duration-300"
       >
-        Sigup
+        {text2}
       </Link>
     </div>
   );
