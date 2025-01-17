6d4b7eb8c7cfa0e963f4bb0aae34ffd809c34d6b
shitoletushar3132 <shitoletushar3132@gmail.com>
2025-01-17 23:09:01 +0530

    onrender

--- client/src/helper/constant.ts
+++ client/src/helper/constant.ts
@@ -1,1 +1,5 @@
-export const BASEURL: string = "https://dev-tinder-back.vercel.app";
+export const BASEURL: string = "https://devtinder-mbmq.onrender.com";
+
+// https://devtinder-mbmq.onrender.com
+
+// https://dev-tinder-back.vercel.app
