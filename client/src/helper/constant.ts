6be3a2d06ab6472ce8abfb2983f366c3c54b7214
shitoletushar3132 <shitoletushar3132@gmail.com>
2025-01-22 22:49:28 +0530

    base ur;

--- client/src/helper/constant.ts
+++ client/src/helper/constant.ts
@@ -4,9 +4,9 @@ export const createSocketConnection = () => {
   return io(BASEURL);
 };
 
-export const BASEURL: string = "http://localhost:3000";
+export const BASEURL: string = "https://devtinder-mbmq.onrender.com";
 
 //https://devtinder-mbmq.onrender.com
-//
+//http://localhost:3000
 
 // https://dev-tinder-back.vercel.app
