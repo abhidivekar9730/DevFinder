790b5fd9cb35ee08faf6a0a00ee90c05f40e7ab5
Abhishek Divekar <abhishekdivekar4829@gmail.com>
2025-02-06 19:46:21 +0530

    server

--- client/src/helper/constant.ts
+++ client/src/helper/constant.ts
@@ -4,7 +4,7 @@ export const createSocketConnection = () => {
   return io(BASEURL);
 };
 
-export const BASEURL: string = "http://localhost:3000";
+export const BASEURL: string = "https://devfinder03.onrender.com";
 
 //https://devtinder-mbmq.onrender.com
 //
